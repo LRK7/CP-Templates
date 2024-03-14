4a8bde0870e16c6fe0db5a5169f39f9fe54e2498
LRK7 <163456036+LRK7@users.noreply.github.com>
2024-03-15 00:07:56 +0530

    simple_sieve.cpp

---
+++ simple_sieve.cpp
@@ -0,0 +1,13 @@
+typedef long long ll;
+
+vector<bool> simple_sieve(ll n) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (ll i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (ll j = i * i; j <= n; j += i)
+                is_prime[j] = false;
+        }
+    }
+    return is_prime;
+}

5e63ba93b89d9237bbf5c6c44c4d683beff19f8d
LRK7 <163456036+LRK7@users.noreply.github.com>
2024-03-29 23:43:27 +0530

    Create spf_using_simple_sieve.cpp

---
+++ spf_using_simple_sieve.cpp
@@ -0,0 +1,17 @@
+typedef long long ll;
+
+vector<ll> spf_using_simple_sieve(ll n) {
+    vector<ll> spf(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+        spf[i] = i;
+    }
+    for (ll i = 2; i * i <= n; i++) {
+        if (spf[i] == i) {
+            for (ll j = i * i; j <= n; j += i)
+                if (spf[j] > i) {
+                    spf[j] = i;
+                }
+        }
+    }
+    return spf;
+}
